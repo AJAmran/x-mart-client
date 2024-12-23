815ae3d31b7029a644066aa5fd6957a1e618e10d
AJAmran <mdamranhossen77@gmail.com>
2024-12-23 11:56:47 +0600

    banner added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
+import Hero from '@/components/Hero';
 import React from 'react';
 
 export default function Home() {
   return (
-    <section className="px-4 py-10 bg-background">
-      <h1 className="text-4xl font-bold text-primary">Welcome to X-Mart!</h1>
-      <p className="mt-4 text-mutedText">Your one-stop solution for groceries and more!</p>
-    </section>
+    <>
+    <Hero />
+    </>
   );
 }
