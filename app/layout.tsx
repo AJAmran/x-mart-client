d155951acf9070de2ce5a9dea0c6f0860853e394
AJAmran <mdamranhossen77@gmail.com>
2024-12-26 18:06:57 +0600

    updated ui lib

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
-import "../styles/globals.css";
+import "@/styles/globals.css";
 import { Metadata, Viewport } from "next";
 import { Link } from "@nextui-org/link";
 import clsx from "clsx";
 
-import { fontSans } from "../config/fonts";
-import { siteConfig } from "../config/site";
-import { Providers } from "../lib/Providers";
-import { Navbar } from "../components/navbar";
+import { Providers } from "./providers";
+
+import { siteConfig } from "@/config/site";
+import { fontSans } from "@/config/fonts";
+import { Navbar } from "@/components/navbar";
 
 export const metadata: Metadata = {
   title: {
@@ -37,7 +38,7 @@ export default function RootLayout({
       <body
         className={clsx(
           "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
+          fontSans.variable,
         )}
       >
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,10 @@ import { Link } from "@nextui-org/link";
 import { Snippet } from "@nextui-org/snippet";
 import { Code } from "@nextui-org/code";
 import { button as buttonStyles } from "@nextui-org/theme";
-import { subtitle, title } from "../components/primitives";
-import { siteConfig } from "../config/site";
-import { GithubIcon } from "../components/icons";
+
+import { siteConfig } from "@/config/site";
+import { title, subtitle } from "@/components/primitives";
+import { GithubIcon } from "@/components/icons";
 
 export default function Home() {
   return (
