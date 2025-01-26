08b944b3337f753af8903adc81c402afad91a736
AJAmran <mdamranhossen77@gmail.com>
2025-01-26 15:13:21 +0600

    updated dashboard

--- src/app/(withRoute)/layout.tsx
+++ src/app/(withRoute)/layout.tsx
@@ -1,7 +1,17 @@
+import Footer from "@/src/components/UI/Footer";
+import { Navbar } from "@/src/components/UI/navbar";
 import React from "react";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <div className="relative flex flex-col h-screen">
+      <Navbar />
+      <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
 };
 
 export default layout;
